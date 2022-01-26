bb6a53f83151301ce08a5c9f951915e2d5d75ef1
Neverrus <neverrushs@gmail.com>
2022-01-26 19:14:10 +0300

    minor updates

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -5,15 +5,5 @@ class PostCreate(forms.Form):
     title = forms.CharField(max_length=200)
     image = forms.ImageField(required=False)
     slug = forms.SlugField()
-    text = forms.CharField(max_length=2000, help_text='Введите ваш текст')
+    text = forms.CharField(max_length=2000, widget=forms.Textarea, help_text='Введите ваш текст')
 
-"""
-from django import forms
-
-
-class PostForm(forms.Form):
-    title = forms.CharField(max_length=200)
-    image = forms.ImageField(required=False)
-    slug = forms.SlugField()
-    text = forms.CharField(max_length=2000, help_text='Введите ваш текст')
-"""
