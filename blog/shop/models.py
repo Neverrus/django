14c25acb3fc23e698694226c976554cdb316fe2c
Neverrus <neverrushs@gmail.com>
2022-02-09 21:58:54 +0300

    add classwork

--- blog/shop/models.py
+++ blog/shop/models.py
@@ -1,10 +1,29 @@
 from django.conf import settings
 from django.db import models
 
+STATUS_CHOICES = (("IN_STOCK", "In Stock"), ("OUT_OF_STOCK", "Out Of Stock"))
+
+ORDER_BY_CHOICES = (
+    ("cost_asc", "Cost Asc"),
+    ("cost_desc", "Cost Desc"),
+    ("max_count", "Max Count"),
+    ("max_price", "Max Cost"),
+)
+
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
+    external_id = models.IntegerField(null=True, blank=True)
     cost = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
+    status = models.CharField(
+        max_length=100, choices=STATUS_CHOICES, default="IN_STOCK"
+    )
+
+    def __str__(self):
+        return f"{self.title} - {self.cost}"
 
 
 class Purchase(models.Model):
